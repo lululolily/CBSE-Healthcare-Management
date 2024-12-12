c924f4877a11c71357bb4c4c057ab336ed734e5e
Emilia <66918373+lululolily@users.noreply.github.com>
2024-12-13 05:51:54 +0800

    spring initializer

---
+++ src/main/java/org/cbse/project/healthcare_management/HealthcareManagementApplication.java
@@ -0,0 +1,21 @@
+package org.cbse.project.healthcare_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HealthcareManagementApplication {
+
+	@GetMapping("/hello")
+    public String check() {
+        return "Hello World There";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareManagementApplication.class, args);
+	}
+
+}
